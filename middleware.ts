0177e628ba28939235d6afc8fa5891f72576dfce
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2025-02-06 17:38:16 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(
-  ['/payment']
-)
-  export default clerkMiddleware (async (auth, req) => {
-    if (isProtectedRoute(req))await auth.protect()
-    })
+// Define the protected route matcher
+const isProtectedRoute = createRouteMatcher(['/payment']);
 
+export default clerkMiddleware(async (auth, req) => {
+  // If the current request matches the protected route, ensure the user is authenticated
+  if (isProtectedRoute(req)) {
+    await auth.protect();  // Protect the route with Clerk authentication
+  }
+});
 
+// Define the config for the matcher
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
